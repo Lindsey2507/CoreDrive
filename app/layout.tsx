7694265ba9456ce07c0428067b1ac7fc3837de84
lindsey2507 <phamthaodk@gmail.com>
2026-03-09 03:09:41 +0900

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Poppins, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+})
+
+export const metadata: Metadata = {
+  title: "Cloudly",
+  description: "Cloudly - Store, share, and collaborate without limits",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("font-sans", geist.variable)}>
+      <body
+        className={`${poppins.variable} font-poppins antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">
+        Cloudly - Store, share, and collaborate without limits
+      </h1>
+    </div>
+  );
+}
